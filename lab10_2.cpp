24ddba42e37bb12d5a925373bffe7b71f618fd08
THanyaTHornnn <woonnasena49@gmail.com>
2024-12-17 17:26:40 +0700

    complete

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -8,6 +8,13 @@ int main (){
 	ofstream dest;
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
+	string text;
+	dest << "-------------------- BOOM ---------------------"<< endl;
+	while(getline(source,text)){
+		dest << text << endl;
+	} 
+	dest << "-------------------- HA!! ---------------------" << endl;
+
 	
 	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,28 @@
 //cout << "Number of data = ";
 //cout << "Mean = ";
 //cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main(){
+    int count = 0;
+    float sum = 0;
+    float sumsq = 0;
+    string text;
+    ifstream source("score.txt");
+    while (getline(source,text))
+    {
+      sum = sum + atof(text.c_str());
+      sumsq = sumsq + pow(atof(text.c_str()),2);
+      count++;
+    }
+    
+    cout << "Number of data = " << count << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << sum/count << endl;
+    cout << "Standard deviation = " << sqrt((sumsq/count)-pow(sum/count,2));
+}
